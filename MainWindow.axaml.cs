77a8183145ee09591413b261cf8b93e8995a4b21
Adriano D'Alonzo <adrianodalonzo@gmail.com>
2025-05-05 08:32:39 -0400

    Move all log files to a unique directory for easy access + keeps all logs for specified system

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -200,10 +200,13 @@ public partial class MainWindow : Window
             // Initialize/clear the log file
             _logFilePath = Path.Combine(
                 Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
-                (bool)CsvOutputCheckbox.IsChecked ? $"{IPAddressInput.Text}-RatesAverage.csv" : $"{IPAddressInput.Text}-RatesAverage.txt");
+                $"ShowRatesLoggerGUI_{IPAddressInput.Text}"
+            );
 
-            if (File.Exists(_logFilePath))
-                File.Delete(_logFilePath);
+            if(!Directory.Exists(_logFilePath))
+                Directory.CreateDirectory(_logFilePath);
+
+            _logFilePath += (bool)CsvOutputCheckbox.IsChecked ? $"\\{DateTime.Now:yyyy-MM-dd_HH_mm_ss}-RatesAverage.csv" : $"\\{DateTime.Now:yyyy-MM-dd_HH_mm_ss}-RatesAverage.txt";
 
             // Create initial empty file
             if ((bool)CsvOutputCheckbox.IsChecked)
@@ -292,7 +295,7 @@ public partial class MainWindow : Window
         IPAddressInput.IsEnabled = true;
         ConnectButton.IsEnabled = true;
 
-        if (!wallNotStarted) { UpdateRunStatus($"Stopped logging after {Math.Round(_stopwatch.Elapsed.TotalSeconds, 2)} second(s) [{Math.Round(_stopwatch.Elapsed.TotalMinutes, 2)} minute(s)]", Brushes.White); }
+        if (!wallNotStarted) { UpdateRunStatus($"Stopped logging after {Math.Round(_stopwatch.Elapsed.TotalMinutes, 2)} minute(s)", Brushes.White); }
         else { UpdateRunStatus("Wall not started", Brushes.Red); }
     }
 
